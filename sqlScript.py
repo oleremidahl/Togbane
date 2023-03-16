cef81fe23895c3bb645a2c3d127d312f3b313d5f
vidarsgl <vidarsgl@stud.ntnu.no>
2023-03-16 13:42:22 +0100

    ahhhh

--- sqlScript.py
+++ sqlScript.py
@@ -1,7 +1,7 @@
 import sqlite3
-con = sqlite3.connect('jernbane.db')
+con = sqlite3.connect('jb.db')
 cursor = con.cursor()
-cursor.execute("SELECT * FROM JernbaneStasjon")
+cursor.execute("SELECT * FROM TogRuteForekomst")
 p = cursor.fetchall()
 print(p)
 con.close()
