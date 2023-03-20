46aef10ea11c995a19efd90830f5abcf1367b9f9
Ole Remi Dahl <olerd@stud.ntnu.no>
2023-03-20 12:43:01 +0100

    Bedre bhC

--- Brukerhistorie_C.py
+++ Brukerhistorie_C.py
@@ -1,21 +1,19 @@
 import sqlite3
-con = sqlite3.connect('.db')
+con = sqlite3.connect('jernbane.db')
 cursor = con.cursor()
 
 stasjon = input("Velg en stasjon: ")
 ukedag = input("Velg en ukedag: ")
 
-cursor.execute('''f'
-        SELECT TogRuteForekomst.ruteNr
+cursor.execute('''
+        SELECT TogRuteForekomst.ruteNr, ForekomstDato.ukedag, StasjonPaaRute.stasjonsNavn
         FROM ((ForekomstDato 
         INNER JOIN 
         TogRuteForekomst ON ForekomstDato.dato == TogRuteForekomst.dato)
         INNER JOIN 
         StasjonPaaRute ON TogRuteForekomst.ruteNr == StasjonPaaRute.ruteNr)
-        WHERE ForekomstDato.ukedag == {ukedag} AND StasjonPaaRute.stasjonsNavn == {stasjon}
-        '
-        '''
-)
+        WHERE ForekomstDato.ukedag = ? AND StasjonPaaRute.stasjonsNavn = ?
+        ''',(ukedag, stasjon))
 
 p = cursor.fetchall()
 print(p)
