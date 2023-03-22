a5f1886c9b5cd349a34259d1f38fb7f15c1d0e58
vidarsgl <vidarsgl@stud.ntnu.no>
2023-03-22 13:06:50 +0100

    Fix (bhE)

--- Brukerhistorie_E.py
+++ Brukerhistorie_E.py
@@ -1,7 +1,23 @@
 import sqlite3
+
 con = sqlite3.connect('jernbane.db')
 cursor = con.cursor()
 
+# Sjekker om epost og telefonnummer allerede eksisterer i databasen
+while True:
+    navn = input("Skriv inn navn: ")
+    email = input("Skriv inn email: ")
+    tlf = input("Skriv inn tlfnummer: ")
+    
+    cursor.execute("SELECT * FROM Kunde WHERE email = ? OR tlf = ?", (email, tlf))
+    eksisterende_kunde = cursor.fetchone()
+    
+    if eksisterende_kunde is not None:
+        print("En kunde med samme email eller tlfnummer eksisterer allerede i databasen.")
+    else:
+        break
+
+# Oppretter ny kunde
 cursor.execute("SELECT MAX(kundeNr) FROM Kunde")
 max_kundeNr = cursor.fetchone()[0]
 
@@ -10,14 +26,12 @@ if max_kundeNr is None:
 else:
     kundeNr = max_kundeNr + 1
 
-navn = input("Skriv inn navn: ")
-epost = input("Skriv inn epost: ")
-telefon = input("Skriv inn telefonnummer: ")
-
-cursor.execute("INSERT INTO Kunde VALUES(?,?,?,?)", (kundeNr, navn, epost, telefon))
+cursor.execute("INSERT INTO Kunde VALUES(?,?,?,?)", (kundeNr, navn, email, tlf))
 con.commit()
 
+# Viser alle kunder i databasen
 cursor.execute("SELECT * FROM Kunde")
 p = cursor.fetchall()
 print(p)
+
 con.close()
